c55971c15ff9b20d2c2cb8d6f9176a51d5c27bf9
RAGUL S <ragulkumar3153@gmail.com>
2024-12-29 19:51:05 +0530

    CSS added

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -29,24 +29,28 @@ function Favourite(props){
     return(
         <div>
             <Header/>
-            <button onClick={addfavorite}>Student List</button>
-            <button>Favourites</button>
-
+            <div className="flex text-2xl font-bold justify-between p-5 bg-yellow-400">
+            <button className="bg-green-600 border border-transparent rounded-xl p-3" onClick={addfavorite}>Student List</button>
+            <h1 className="border border-transparent rounded-xl bg-green-600 p-3 ">Favourites</h1>
+            </div>
+            <div className="bg-gray-600">
              {
                 studentlist.map(function(item,index)
                 {
                     return (
-                        <div>
-                             
-                             
+                        <div>    
                              {
-                                item.favourite == true ? <div><h2>{index+1}.{item.name}</h2><button onClick={()=>{handleremove(item.id)}}>Remove</button></div> : <div></div>
+                                item.favourite == true ? <div className="flex text-1xl font-bold justify-between p-5">
+                                <h2 className="bg-red-400 p-5 border border-transparent rounded-md">{index+1}.{item.name}</h2>
+                                <button className="bg-blue-400 p-5 border rounded-md" onClick={()=>{handleremove(item.id)}}>Remove</button>
+                                </div> : <div></div>
                              }
                         </div>
                     )
                     
                 })
             }
+            </div>
         </div>
     )
 }
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -32,18 +32,19 @@ function Student(props){
     return(
         <div>
             <Header/>
-            <div className="flex text-2xl font-bold justify-between p-5">
-            <h1 className="border border-black bg-green-400 p-3">Student List</h1> <button className="bg-green-400 border border-black p-3" onClick={addfavorite}>Favourites</button>
+            <div className="flex text-2xl font-bold justify-between p-5 bg-yellow-400">
+            <h1 className="border border-transparent rounded-xl bg-green-600 p-3 ">Student List</h1> 
+            <button className="bg-green-600 border border-transparent rounded-xl p-3" onClick={addfavorite}>Favourites</button>
             </div>
-            <div className="flex flex-col">
+            <div className="bg-gray-600">
             {
                 studentlist.map(function(item,index)
                 {
                     return (
-                        <div className="flex gap-10">
-                            <h2>{index+1}.{item.name}</h2>
+                        <div className="flex text-1xl font-bold justify-between p-5">
+                            <h2 className="bg-red-400 p-2 border border-transparent rounded-md">{index+1}.{item.name}</h2>
                             {
-                                item.favourite == false ? <button onClick={()=>{handleclick(item.id)}}>Add to Favourite</button> : <button>Add to Favourite</button>
+                                item.favourite == false ? <button className="bg-blue-400 p-2 border rounded-md" onClick={()=>{handleclick(item.id)}}>Add to Favourite</button> : <button className="bg-red-400 p-2 border rounded-md">Add to Favourite</button>
                             }
                             
                         </div>
