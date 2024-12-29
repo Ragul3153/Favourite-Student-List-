4dece28f1c3a9ddd33dda3557c6ea08bc370e906
RAGUL S <ragulkumar3153@gmail.com>
2024-12-29 21:06:57 +0530

    CSS Updated

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -29,18 +29,18 @@ function Favourite(props){
     return(
         <div>
             <Header/>
-            <div className="flex text-2xl font-bold justify-between p-5 bg-gray-400 m-5 md:mx-[550px] border rounded-md">
+            <div className="flex text-2xl font-bold justify-between p-5 bg-gray-400 m-5 md:justify-center md:gap-32 border rounded-md">
             <button className="bg-yellow-600 border border-transparent rounded-xl p-3" onClick={addfavorite}>Student List</button>
             <h1 className="border border-transparent rounded-xl bg-yellow-600 p-3 ">Favourites</h1>
             </div>
-            <div className="bg-gray-400 m-5 border-rounded-md pb-60 md:mx-[550px] border rounded-md">
+            <div className="bg-gray-400 m-5 border-rounded-md pb-60 border rounded-md">
              {
                 studentlist.map(function(item,index)
                 {
                     return (
                         <div>    
                              {
-                                item.favourite == true ? <div className="flex text-1xl font-bold justify-between p-5">
+                                item.favourite == true ? <div className="flex text-1xl font-bold justify-between p-5 md:justify-center md:gap-32">
                                 <h2 className="bg-green-600 w-32 p-2 text-center border border-transparent rounded-md">{item.name}</h2>
                                 <button className="bg-blue-500 w-32 p-2 border rounded-md border-transparent" onClick={()=>{handleremove(item.id)}}>Remove</button>
                                 </div> : <div></div>
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -32,11 +32,11 @@ function Student(props){
     return(
         <div>
             <Header/>
-            <div className="flex text-2xl font-bold justify-between p-5 bg-gray-400 m-5 md:mx-[550px] border rounded-md">
+            <div className="flex text-2xl font-bold justify-between p-5 bg-gray-400 m-5 md:justify-center md:gap-32 border rounded-md">
             <h1 className="border border-transparent rounded-xl bg-yellow-500 p-3 ">Student List</h1> 
             <button className="bg-yellow-500 border border-transparent rounded-xl p-3" onClick={addfavorite}>Favourites</button>
             </div>
-            <div className="bg-gray-400 m-5 border-rounded-md pb-60 md:mx-[550px] border rounded-md">
+            <div className="bg-gray-400 m-5 border-rounded-md pb-60 md:justify-center md:gap-32 border rounded-md">
             {
                 studentlist.map(function(item)
                 {
